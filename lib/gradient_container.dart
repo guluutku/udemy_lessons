9d3867e14db7f81511199011f3b81b3a871cd7dc
Gün ULUUTKU <gunuluutku@gmail.com>
2023-05-04 20:09:39 +0300

    variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:udemy_lessons/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({
@@ -11,14 +13,15 @@ class GradientContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98),
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )),
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
       child: const Center(
         child: StyledText(),
       ),
