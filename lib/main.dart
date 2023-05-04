d7310f2fa9ee70ee94a0f9f578cefe86d6c61ff8
Gün ULUUTKU <gunuluutku@gmail.com>
2023-05-04 19:36:17 +0300

    Removed appbar

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key,});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -31,9 +30,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Container(
         decoration: const BoxDecoration(
             gradient: LinearGradient(
