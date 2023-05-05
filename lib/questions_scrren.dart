18b3bbc231750f678125704572f093bcca466ede
Gün ULUUTKU <gunuluutku@gmail.com>
2023-05-05 18:42:11 +0300

    Custom button

--- lib/questions_scrren.dart
+++ lib/questions_scrren.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_lessons/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +11,38 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The Question....',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: 'Answer1..',
+            onPressed: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer2..',
+            onPressed: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer3..',
+            onPressed: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer4..',
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
