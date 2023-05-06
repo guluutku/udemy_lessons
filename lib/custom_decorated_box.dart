ff6dbbb1543d84e14486ea119df911e3ecd1ff42
Gün ULUUTKU <gunuluutku@gmail.com>
2023-05-06 18:27:17 +0300

    Aling colors

--- lib/custom_decorated_box.dart
+++ lib/custom_decorated_box.dart
@@ -15,6 +15,8 @@ class CustomDecoratedBox extends StatelessWidget {
             Color.fromARGB(255, 78, 13, 151),
             Color.fromARGB(255, 107, 15, 168),
           ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
       child: child,
