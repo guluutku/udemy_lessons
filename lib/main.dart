d2699996718319e035a00f2a1e4ade0ba98ddddd
Gün ULUUTKU <gunuluutku@gmail.com>
2023-05-14 17:12:10 +0300

    New class added, refactored folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:udemy_lessons/expenses.dart';
+import 'package:udemy_lessons/widgets/expenses.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Expenses(),
+      home:  Expenses(),
     );
   }
 }
