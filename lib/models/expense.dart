cc04d5717fd99ec43ad76dbe373d935516d0b012
Gün ULUUTKU <gunuluutku@gmail.com>
2023-05-14 16:30:13 +0300

    Made models with unique ids

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,4 +1,16 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
 
 class Expense {
-  
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.purchaseDate,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime purchaseDate;
 }
