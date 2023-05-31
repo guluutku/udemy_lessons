6077ed618ce2edcd2c9836cd87125ef4627db122
Gün ULUUTKU <gunuluutku@gmail.com>
2023-05-31 18:10:35 +0300

    Lock UI to Portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:udemy_lessons/themes.dart';
 import 'package:udemy_lessons/widgets/expenses.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((value) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); 
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
