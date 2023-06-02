5117c24ed3eaa1d9afe5e62fdcc0fe4643f76d01
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-02 20:00:04 +0300

    Show dummy data in screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_lessons/data/dummy_data.dart';
+import 'package:udemy_lessons/screens/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -16,16 +18,11 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text('1', style: TextStyle(color: Colors.white),),
-          Text('2', style: TextStyle(color: Colors.white),),
-          Text('3', style: TextStyle(color: Colors.white),),
-          Text('4', style: TextStyle(color: Colors.white),),
-          Text('5', style: TextStyle(color: Colors.white),),
-          Text('6', style: TextStyle(color: Colors.white),),
-          Text('7', style: TextStyle(color: Colors.white),),
-          Text('8', style: TextStyle(color: Colors.white),),
-          Text('9', style: TextStyle(color: Colors.white),),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+            ),
         ],
       ),
     );
