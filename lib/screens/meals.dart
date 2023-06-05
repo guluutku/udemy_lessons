46848fcfb557843dad66f56344ab9b3a13137f90
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-05 21:35:40 +0300

    Body content added

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -14,13 +14,42 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-      itemBuilder: (ctx, index) => Text(''),
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => Text(
+        meals[index].title,
+      ),
     );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'No meal',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Try selecting real category',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
+      body: content,
     );
   }
 }
