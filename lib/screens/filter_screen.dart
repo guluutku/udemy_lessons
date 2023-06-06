887b5310e20554b2f657f1b25056edec78c93bb4
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-06 20:17:15 +0300

    All filters added

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -9,6 +9,9 @@ class FilterScreen extends StatefulWidget {
 
 class _FilterScreenState extends State<FilterScreen> {
   var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _vegetarianFilterSet = false;
+  var _veganFilterSet = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +29,80 @@ class _FilterScreenState extends State<FilterScreen> {
       */
       body: Column(
         children: [
-          SwitchListTile(
-            value: _glutenFreeFilterSet,
-            onChanged: (value) {
-              setState(() {
-                _glutenFreeFilterSet = value;
-              });
-            },
-            title: Text(
-              'Gluten Free',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-          ),
+          glutenFilter(context),
+          lactoseFreeFilter(context),
+          vegetarianFilter(context),
+          veganFilter(context),
         ],
       ),
     );
+  }
+
+  SwitchListTile veganFilter(BuildContext context) {
+    return SwitchListTile(
+          value: _veganFilterSet,
+          onChanged: (value) {
+            setState(() {
+              _veganFilterSet = value;
+            });
+          },
+          title: Text(
+            'Vegan',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        );
+  }
+
+  SwitchListTile vegetarianFilter(BuildContext context) {
+    return SwitchListTile(
+          value: _vegetarianFilterSet,
+          onChanged: (value) {
+            setState(() {
+              _vegetarianFilterSet = value;
+            });
+          },
+          title: Text(
+            'Vegetarian',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        );
+  }
+
+  SwitchListTile lactoseFreeFilter(BuildContext context) {
+    return SwitchListTile(
+          value: _lactoseFreeFilterSet,
+          onChanged: (value) {
+            setState(() {
+              _lactoseFreeFilterSet = value;
+            });
+          },
+          title: Text(
+            'Lactose Free',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        );
+  }
+
+  SwitchListTile glutenFilter(BuildContext context) {
+    return SwitchListTile(
+          value: _glutenFreeFilterSet,
+          onChanged: (value) {
+            setState(() {
+              _glutenFreeFilterSet = value;
+            });
+          },
+          title: Text(
+            'Gluten Free',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        );
   }
 }
