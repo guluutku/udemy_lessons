6c9b94cf7612b1f05a8d18366cbbaa437db2bc68
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-06 20:28:54 +0300

    Read and use returned data

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -52,11 +52,15 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => const FilterScreen()));
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
+        MaterialPageRoute(
+          builder: (context) => const FilterScreen(),
+        ),
+      );
+       print(result);
     }
   }
 
