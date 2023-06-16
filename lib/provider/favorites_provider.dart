9d117a743dcd409144c671a01d6bbb57ce6a253b
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-16 11:41:51 +0300

    Made state notifier for complex provider

--- lib/provider/favorites_provider.dart
+++ lib/provider/favorites_provider.dart
@@ -1,3 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:udemy_lessons/model/meal.dart';
 
-final favoritesProvider = Provider((ref) {});
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleFavoritesState(Meal meal) {
+    final isFavorite = state.contains(meal);
+    if (isFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoritesProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
