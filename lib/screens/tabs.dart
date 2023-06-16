6d9b53f1dea1968d82a85f6f012b906c66c81f3b
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-16 11:51:26 +0300

    Unnecessary import removed

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:udemy_lessons/model/meal.dart';
 import 'package:udemy_lessons/provider/favorites_provider.dart';
 import 'package:udemy_lessons/provider/meals_provider.dart';
 import 'package:udemy_lessons/screens/categories.dart';
