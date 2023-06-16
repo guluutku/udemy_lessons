b1c91bede43a6a39ba8185be6414e429858c32c2
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-16 11:57:54 +0300

    toggle returns bool

--- lib/provider/favorites_provider.dart
+++ lib/provider/favorites_provider.dart
@@ -4,12 +4,14 @@ import 'package:udemy_lessons/model/meal.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  void toggleFavoritesState(Meal meal) {
+  bool toggleFavoritesState(Meal meal) {
     final isFavorite = state.contains(meal);
     if (isFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
+      return true;
     } else {
       state = [...state, meal];
+      return false;
     }
   }
 }
