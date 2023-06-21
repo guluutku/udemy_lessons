0cd3702bc488f6da79060176d09db278865f854d
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-21 22:44:02 +0300

    Changed to stateful widget and animation controller added

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,15 +6,39 @@ import 'package:udemy_lessons/screens/meals.dart';
 
 import '../model/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     required this.availableMeals,
   });
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectedCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals.where((element) {
+    final filteredMeals = widget.availableMeals.where((element) {
       return element.categories.contains(category.id);
     }).toList();
 
