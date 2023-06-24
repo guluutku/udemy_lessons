b8cc232fef78a1fcfa456fb1829604c823ab9325
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-24 14:49:48 +0300

    New screen added

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_lessons/data/dummy_items.dart';
+import 'package:udemy_lessons/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItemPage() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => const NewItem()),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItemPage,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            height: 24,
+            width: 24,
+            color: groceryItems[index].category.color,
+          ),
+          trailing: Text(
+            groceryItems[index].quantity.toString(),
+          ),
+        ),
+      ),
+    );
   }
 }
