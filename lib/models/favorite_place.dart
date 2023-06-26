3c1c8e4979d4940522766e38dc1336a6a4b4b0d1
Gün ULUUTKU <gunuluutku@gmail.com>
2023-06-26 16:05:50 +0300

    uuid Added

--- lib/models/favorite_place.dart
+++ lib/models/favorite_place.dart
@@ -1,7 +1,12 @@
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
 class FavoritePlace {
   FavoritePlace({
     required this.title,
-  });
+  }) : id = uuid.v4();
 
+  final String id;
   final String title;
 }
