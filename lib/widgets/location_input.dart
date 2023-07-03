d790bbe003b0a90f341119a402c4ffd87993b3ea
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-03 10:04:08 +0300

    StatefulWidget class created

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocationInputWidget extends StatefulWidget {
+  const LocationInputWidget({super.key});
+
+  @override
+  State<LocationInputWidget> createState() => _LocationInputWidgetState();
+}
+
+class _LocationInputWidgetState extends State<LocationInputWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
