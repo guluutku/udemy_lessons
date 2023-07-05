694c949de1625140126cea31255f4e0431352830
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-05 18:23:21 +0300

    Show location in details screen

--- lib/screens/place_details_screen.dart
+++ lib/screens/place_details_screen.dart
@@ -11,6 +11,10 @@ class FavoritePlaceDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context)
+        .textTheme
+        .titleLarge!
+        .copyWith(color: Theme.of(context).colorScheme.onBackground);
     return Scaffold(
       appBar: AppBar(
         title: Text(favoritePlace.title),
@@ -22,6 +26,40 @@ class FavoritePlaceDetailsScreen extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 16,
+              ),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.transparent,
+                    Colors.black,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Latitude: ${favoritePlace.locationModel.latitude}',
+                    style: textTheme,
+                  ),
+                  Text(
+                    'Longitude: ${favoritePlace.locationModel.longitude}',
+                    style: textTheme,
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
