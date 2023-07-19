9c691c7e27c37bbf3a64a6293e6b7ce49f6598fb
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-19 18:22:53 +0300

    Message widgets structure added

---
+++ lib/widgets/new_message_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewMessageWidget extends StatefulWidget {
+  const NewMessageWidget({super.key});
+
+  @override
+  State<NewMessageWidget> createState() => _NewMessageWidgetState();
+}
+
+class _NewMessageWidgetState extends State<NewMessageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
