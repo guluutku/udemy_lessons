6f449eef477205b1be82aeec336804c668dfb9d9
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-19 18:35:40 +0300

    Added text field, clears textfield when a message is sent

--- lib/widgets/new_message_widget.dart
+++ lib/widgets/new_message_widget.dart
@@ -8,8 +8,59 @@ class NewMessageWidget extends StatefulWidget {
 }
 
 class _NewMessageWidgetState extends State<NewMessageWidget> {
+  final _messageController = TextEditingController();
+
+  void _sendMessage() {
+    final enteredMessage = _messageController.text;
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+
+    _messageController.clear();
+  }
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 15,
+        right: 1,
+        bottom: 14,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: _messageTextField(),
+          ),
+          IconButton(
+            onPressed: _sendMessage,
+            icon: Icon(
+              Icons.send,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  TextField _messageTextField() {
+    return TextField(
+      controller: _messageController,
+      autocorrect: true,
+      textCapitalization:
+          TextCapitalization.sentences, // Capitalize first letter of sentences,
+      enableSuggestions: true,
+      decoration: const InputDecoration(
+        label: Text('Message here'),
+      ),
+    );
   }
 }
