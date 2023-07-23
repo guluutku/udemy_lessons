b9063d230f99b75dacc948a59d9f2c5e0585f2ad
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-23 17:35:51 +0300

    Display chat messages
    Only shown sent messages

--- lib/widgets/chat_messages_widget.dart
+++ lib/widgets/chat_messages_widget.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessagesWidget extends StatelessWidget {
@@ -5,6 +7,43 @@ class ChatMessagesWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'createdAt',
+            descending: false,
+          )
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No Messages Found'),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        }
+        final messages = snapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: messages.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Text(
+              messages[index].data()['text'],
+            );
+          },
+        );
+      },
+    );
   }
 }
